fc71738994a59e2565a15ea9b28dc5a826f57933
onchangelz <2235841385@qq.com>
2015-06-07 16:40:56 +0800

    second commit

--- eventUtil.js
+++ eventUtil.js
@@ -75,11 +75,22 @@ var eventUtil = {
             return -event.detail * 40;
         }
     },
-    getCharCode:function(event){
-        if(typeof event.charCode=="number"){
+    getCharCode: function(event) {
+        if (typeof event.charCode == "number") {
             return event.charCode;
-        }else{
+        } else {
             return event.keyCode;
+        }
+    },
+    getClipboardText: function(event) {
+        var clipboardData = (event.clipboardData || window.clipboardData);
+        return clipboardData.getData("text");
+    },
+    setClipboardText: function(event, value) {
+        if (event.clipboardData) {
+            return event.clipboardData.setData("text/plain", value);
+        } else if (window.clipboardData) {
+            return window.clipboardData.setData("text", value);
         }
     }
 };

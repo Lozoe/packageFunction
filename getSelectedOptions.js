3fd2c41bfe545c729fc09c7e14517795de38c58a
onchangelz <2235841385@qq.com>
2015-06-09 17:51:49 +0800

    add getSelectedOptions function

---
+++ getSelectedOptions.js
@@ -0,0 +1,12 @@
+//传入一个select元素的引用，返回一个选中选项应用的数组
+function getSelectedOptions(selectBox) {
+	var result = new Array();
+	var option = null;
+	for (var i = 0, len = selectBox.length; i < len; i++) {
+		option = selectBox.options[i];
+		if (option.selected) {
+			result.push(option);
+		}
+	}
+	return result;
+}
